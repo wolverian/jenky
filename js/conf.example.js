400a461672bf831bc1e9281231cda7b1d89193dd
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-10-24 14:43:51 +0300

    Remove conf.js and introduce an example conf.

---
+++ js/conf.example.js
@@ -0,0 +1,13 @@
+(function() {
+    var seconds = 1000;
+    window.jenky = {};
+    window.jenky.conf = {
+        jenkins: {
+            url: 'change me',
+            updateInterval: 5 * seconds
+        },
+        jenky: {
+            updateInterval: 10 * seconds
+        }
+    };
+}(window));
